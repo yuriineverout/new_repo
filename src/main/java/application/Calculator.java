ed84d55fa1690bb828304b73e04c3296285bfc0d
yura_ <yuriineverout@gmail.com>
2021-03-16 20:27:12 +0200

    tests from simple to hard

--- src/main/java/application/Calculator.java
+++ src/main/java/application/Calculator.java
@@ -8,6 +8,28 @@ public class Calculator {
         return a + b;
     }
 
+    public int subtraction(int a, int b) {
+        return a - b;
+
+    }
+
+    public int multiplication(int a, int b) {
+        return a * b;
+    }
+
+    public int division(int a, int b) {
+        return a / b;
+    }
+
+    public double squareRoot(int a) {
+        return Math.sqrt(a);
+    }
+
+    public double power (int base, int component) {
+        return Math.pow(base, component);
+    }
+
+
     //TODO add method for subtraction
 
     //TODO add method for multiplication
@@ -22,16 +44,48 @@ public class Calculator {
 
     //TODO (*) |average value| add method which calculate average value of 3 numbers
 
+    public static double avr(double a, double b, double c) {
+        return(a + b + c) / 3;
+    }
+
+
     //TODO (*) |even or odd| add method which get 1 number as parameter and return true if it even, and false - if odd
 
+    public static boolean evenOdd(int a) {
+        return a % 2 == 0;
+    }
+
     //TODO (*) |biggest value| add method which get 2 number as parameters and return bigger of them, or 0 if they equals
+    public static int biggestValue (int a, int b) {
+        if (a==b){
+            return 0;
+        }
+        return a>b ? a : b;
+
+    }
 
     //***************Hard methods************************
 
     //TODO (**) |addition with multiple parameters| add method which return sum of any quantity of parameter
 
+    public static int sumAll(int...numbers){
+
+        int result = 0;
+        for(int i = 0 ; i < numbers.length; i++) {
+            result = result + numbers[i];
+        }
+        return result;
+    }
+
     //TODO (*) |divide by zero case| add method for division (check division by zero case
     // - if we try to divide by zero - return -100;
+    public static float divide (int a, int b) {
+        if (b==0){
+            return -100;
+        }
+        return a/b;
+
+    }
 
 
 }
--- src/test/java/simpleTests/SimpleTestClass.java
+++ src/test/java/simpleTests/SimpleTestClass.java
@@ -1,20 +1,83 @@
 package simpleTests;
 
 import application.Calculator;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 
 public class SimpleTestClass {
 
-  Calculator calc = new Calculator();
+    Calculator calc = new Calculator();
 
-  @Test
-  public void testAddTwoPositiveValues() {
-    int actualResult = calc.add(10, 5);
-    assertEquals("10 + 5 must be 15", 15, actualResult);
-  }
+    @Test
+    public void testAddTwoPositiveValues() {
+        int actualResult = calc.add(10, 5);
+        assertEquals("10 + 5 must be 15", 15, actualResult);
+    }
 
-  // TODO (*)
-  //add tests for new methods (multiply, division, subtraction, square root, x^2)
+    @Test
+    public void subtraction() {
+        int actualResult = calc.subtraction(21, 10);
+        assertEquals("21 - 10 must be 11", 11, actualResult);
+    }
 
+    @Test
+    public void multiplication() {
+        int actualResult = calc.multiplication(12, 12);
+        assertEquals("12 +12 must be 24", 24, actualResult);
+    }
 
+    @Test
+    public void division (){
+        int actualResult = calc.division(100,50);
+        assertEquals("100/50 must be 2",2, actualResult);
+    }
+    @Test
+    public void aquareRoot () {
+        double actualResult = calc.squareRoot(100);
+        assertEquals("10", 10, actualResult,0.1);
+    }
+    @Test
+    public void power () {
+        double actualResult = calc.power(10,2);
+        assertEquals("100", 100, actualResult, 0.1 );
+    }
+    // TODO (*)
+    //add tests for new methods (multiply, division, subtraction, square root, x^2)
+
+    @Test
+    public void avr() {
+        double actualResult = calc.avr(12,24,54);
+        assertEquals("must be 30", 30, actualResult, 0.1);
+    }
+    @Test
+    public void evenOdd() {
+        boolean actualResult = calc.evenOdd(2);
+        assertEquals("is equal", true, actualResult);
+        actualResult = calc.evenOdd(3);
+        assertEquals("is not equal", false, actualResult);
+    }
+    @Test
+    public void biggestValue() {
+        int actualResult = calc.biggestValue (2, 4);
+        assertEquals("is biggerl", 4, actualResult);
+        actualResult = calc.biggestValue (5, 5);
+        assertEquals("is equal", 0, actualResult);
+
+    }
+    @Test
+    public void sumAll(){
+        int actualResult = calc.sumAll(5,5);
+        assertEquals ("sum of all numbers", 10, actualResult);
+
+    }
+    @Test
+    public void divide (){
+        float actualResult = calc.divide(20, 10);
+        assertEquals ("not zero", 2, actualResult, 0.1);
+        actualResult = calc.divide(20, 0);
+        assertEquals ("with zero", -100, actualResult, 0.1);
+
+    }
 }
